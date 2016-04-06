571c0d4c3321e171fd538011cc69434bacd615ce
whien <sal95610@gmail.com>
2016-04-07 00:06:02 +0800

    webpack server setting completed

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,31 @@ var path = require('path');
 module.exports = {
   entry: ['./source/public/main.js'],
   output: {
-    path: path.resolve(__dirname, 'example/build/'),
+    path: path.resolve(__dirname, 'example/assets/'),
     filename: "bundle.js",
     publicPath: 'example/assets/'
+  },
+  watch: true,
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file"
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
+    ]
   }
 };
